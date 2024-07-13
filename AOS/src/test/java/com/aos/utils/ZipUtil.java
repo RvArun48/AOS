c5032a481963c949a0743c496dca40ab61e2d248
RvArun48 <arun@infohybrid.com>
2024-07-13 19:50:15 +0530

    zip update

--- AOS/src/test/java/com/aos/utils/ZipUtil.java
+++ AOS/src/test/java/com/aos/utils/ZipUtil.java
@@ -4,44 +4,59 @@ import java.io.*;
 import java.util.zip.*;
 
 public class ZipUtil {
-    public static void zipFiles(String zipFileName, String... filePaths) throws IOException {
-        try (FileOutputStream fos = new FileOutputStream(zipFileName);
-             ZipOutputStream zos = new ZipOutputStream(fos)) {
-            
-            for (String filePath : filePaths) {
-                File fileToZip = new File(filePath);
-                if (fileToZip.isDirectory()) {
-                    zipDirectory(fileToZip, fileToZip.getName(), zos);
-                } else {
-                    zipFile(fileToZip, zos);
-                }
-            }
-        }
-    }
+	public static void zipFiles(String zipFileName, String... filePaths) throws IOException {
+		try (FileOutputStream fos = new FileOutputStream(zipFileName); ZipOutputStream zos = new ZipOutputStream(fos)) {
 
-    private static void zipFile(File fileToZip, ZipOutputStream zos) throws IOException {
-        try (FileInputStream fis = new FileInputStream(fileToZip)) {
-        	ZipEntry zipEntry = new ZipEntry(fileToZip.getPath());
-            zos.putNextEntry(zipEntry);
-            
-            byte[] bytes = new byte[1024];
-            int length;
-            while ((length = fis.read(bytes)) >= 0) {
-                zos.write(bytes, 0, length);
-            }
-        }
-    }
+			for (String filePath : filePaths) {
+				File fileToZip = new File(filePath);
+				if (fileToZip.isDirectory()) {
+					zipDirectory(fileToZip, fileToZip.getName(), zos);
+				} else {
+					zipFile(fileToZip, zos, "");
+				}
+			}
+		}
+	}
 
-    private static void zipDirectory(File folderToZip, String folderName, ZipOutputStream zos) throws IOException {
-        File[] files = folderToZip.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    zipDirectory(file, folderName + "/" + file.getName(), zos);
-                } else {
-                    zipFile(new File(folderName + "/" + file.getName()), zos);
-                }
-            }
-        }
-    }
+	private static void zipFile(File fileToZip, ZipOutputStream zos, String parentDirectoryName) throws IOException {
+		if (fileToZip.isHidden()) {
+			return;
+		}
+
+		try (FileInputStream fis = new FileInputStream(fileToZip)) {
+			String zipEntryName = parentDirectoryName + fileToZip.getName();
+			if (parentDirectoryName.isEmpty()) {
+				zipEntryName = fileToZip.getName();
+			}
+			ZipEntry zipEntry = new ZipEntry(zipEntryName);
+			zos.putNextEntry(zipEntry);
+
+			byte[] bytes = new byte[1024];
+			int length;
+			while ((length = fis.read(bytes)) >= 0) {
+				zos.write(bytes, 0, length);
+			}
+		}
+	}
+
+	private static void zipDirectory(File folderToZip, String parentFolder, ZipOutputStream zos) throws IOException {
+		if (folderToZip.isHidden()) {
+			return;
+		}
+
+		File[] files = folderToZip.listFiles();
+		if (files == null || files.length == 0) {
+			zos.putNextEntry(new ZipEntry(parentFolder + "/"));
+			zos.closeEntry();
+			return;
+		}
+
+		for (File file : files) {
+			if (file.isDirectory()) {
+				zipDirectory(file, parentFolder + "/" + file.getName(), zos);
+			} else {
+				zipFile(file, zos, parentFolder + "/");
+			}
+		}
+	}
 }
